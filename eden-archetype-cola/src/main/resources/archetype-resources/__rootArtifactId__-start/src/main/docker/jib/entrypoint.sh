61e0793e04dd61fa0f7b9e3150cb790d3ec3effd
shiyindaxiaojie <1813986321@qq.com>
2022-07-04 23:47:59 +0800

    fixed

--- eden-archetype-cola/src/main/resources/archetype-resources/__rootArtifactId__-start/src/main/docker/jib/entrypoint.sh
+++ eden-archetype-cola/src/main/resources/archetype-resources/__rootArtifactId__-start/src/main/docker/jib/entrypoint.sh
@@ -2,4 +2,4 @@
 #!/bin/sh
 
 echo "The application will start in ${JAVA_SLEEP}s..." && sleep ${JAVA_SLEEP}
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "${groupId}.Application"  "$@"
+exec java ${JAVA_OPTS} ${ENV_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "${groupId}.Application"  "$@"
